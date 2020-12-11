39a11694d48b3b3cfa71685a9ea62effc30fe91c
Umar Saidu Auna <umarmanofpeace@gmail.com>
2020-12-11 09:45:10 +0100

    feature: Make the button created to perform an action

--- app/src/main/java/com/git/gitdemo/MainActivity.java
+++ app/src/main/java/com/git/gitdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.git.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> System.out.println("Hello World"));
     }
 }
